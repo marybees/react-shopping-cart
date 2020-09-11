41b16374cf6f71ab6b8f74610db41abf750b3274
Mary Baker <meb601@gmail.com>
2020-09-10 20:28:04 -0400

    Code addItem function

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Item from './components/ShoppingCartItem';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		const updatedCart = [...cart]
+		updatedCart.push(item)
+		setCart(updatedCart)
 	};
 
 	return (
