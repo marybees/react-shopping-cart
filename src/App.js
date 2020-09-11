dbb613e5c22d7469f72038f4de2f63ce78872ac9
Mary Baker <meb601@gmail.com>
2020-09-10 22:59:38 -0400

    Create and wrap App in CartContext

--- src/App.js
+++ src/App.js
@@ -21,16 +21,18 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
-			</div>
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
